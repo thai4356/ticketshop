123a9e778ab55adb4b83d2a48e14ac7ae7dc2814
zhong xina <117057017+thai4356@users.noreply.github.com>
2024-08-13 20:56:55 +0700

    them

---
+++ src/main/java/web/thaiticketmajor/Models/user/KdlUser.java
@@ -0,0 +1,12 @@
+package web.thaiticketmajor.Models.user;
+
+//package Tên_Công_ty.Tên_Dự_Án.Kdl;
+
+//import Tên_Công_ty.Tên_Dự_Án.Tdl.Thực_Thể;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KdlUser extends JpaRepository<User, Integer>
+{
+
+}
